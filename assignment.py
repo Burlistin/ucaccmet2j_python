0f49e0727b55380d98c074306d2231673f4303fc
Burlistin <ilia005.ilia005@gmail.com>
2026-01-13 12:15:56 +0100

    Part 1: Monthly Precipitation Analysis + Results.json

--- assignment.py
+++ assignment.py
@@ -1,11 +1,12 @@
 import json
 
+from dateutil.relativedelta import relativedelta
+
 #Loading Data
 with open('precipitation.json', encoding='utf-8') as file:
     data = json.load(file)
 
-#Monthly Participation -- Part 1
-
+#Manually selecting for Seattle
 seattle_code = 'GHCND:US1WAKG0038'
 filtered_observations = []
 #Filtering observations based on station code (Seattle)
@@ -23,13 +24,22 @@ for observation in filtered_observations:
     else:
         grouped_observations[month].append(observation)
 
+
 total_monthly_precipitation = []
+total_yearly_precipitation = 0
 #Summarizing precipitation values per month group
 for key in grouped_observations:
     sum_precipitation = 0
     for observation in grouped_observations[key]:
         sum_precipitation += observation['value']
+        #Calculating total yearly precipitation throughout all the months
+        total_yearly_precipitation += observation['value']
     total_monthly_precipitation.append(sum_precipitation)
+
+relative_monthly_precipitation = []
+#Calculating relative monthly precipitation (mutate table)
+for value in total_monthly_precipitation:
+    relative_monthly_precipitation.append(value/total_yearly_precipitation) #unrounded float values
 
 #Finalizing data in the format specified
 results = {
@@ -37,7 +47,9 @@ results = {
         {
             'station': 'GHCND:US1WAKG0038',
             'state': 'WA',
-            'total_monthly_precipitation': total_monthly_precipitation
+            'total_monthly_precipitation': total_monthly_precipitation,
+            'total_yearly_precipitation': total_yearly_precipitation,
+            'relative_monthly_precipitation': relative_monthly_precipitation,
         }
 }
 
