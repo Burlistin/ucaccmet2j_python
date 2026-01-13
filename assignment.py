68e4ff48c80c0407df22af1c95fc8293c176167a
Burlistin <ilia005.ilia005@gmail.com>
2026-01-13 12:06:20 +0100

    Part 1: Monthly Precipitation Analysis + Results.json

--- assignment.py
+++ assignment.py
@@ -1,6 +1,45 @@
 import json
 
+#Loading Data
 with open('precipitation.json', encoding='utf-8') as file:
     data = json.load(file)
 
-print(data)
+#Monthly Participation -- Part 1
+
+seattle_code = 'GHCND:US1WAKG0038'
+filtered_observations = []
+#Filtering observations based on station code (Seattle)
+for observation in data:
+    if observation['station'] == seattle_code:
+        filtered_observations.append(observation)
+
+
+grouped_observations = {}
+#Grouping the filtered observations per month
+for observation in filtered_observations:
+    month = observation['date'].split('-')[1] #Getting the month as a number
+    if month not in grouped_observations:
+        grouped_observations[month] = [observation]
+    else:
+        grouped_observations[month].append(observation)
+
+total_monthly_precipitation = []
+#Summarizing precipitation values per month group
+for key in grouped_observations:
+    sum_precipitation = 0
+    for observation in grouped_observations[key]:
+        sum_precipitation += observation['value']
+    total_monthly_precipitation.append(sum_precipitation)
+
+#Finalizing data in the format specified
+results = {
+    'Seattle':
+        {
+            'station': 'GHCND:US1WAKG0038',
+            'state': 'WA',
+            'total_monthly_precipitation': total_monthly_precipitation
+        }
+}
+
+with open('results.json', 'w', encoding='utf-8') as file:
+    json.dump(results, file, indent=4)
